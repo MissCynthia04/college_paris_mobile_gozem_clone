d29f6943358acebe68224ecb0baa99486f2b94b7
Cynthia <cynamazam@gmail.com>
2024-04-23 12:20:44 +0800

    Home_appBar

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -5,8 +5,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        actions: const [
+          Stack(
+            children: [
+              Positioned.fill(child: Icon(Icons.notifications)),
+              Positioned(child: Text("8"))
+            ],
+          )
+        ],
+        centerTitle: true,
+        title: Image.asset(
+          "assets/images/gozem-logo.png",
+          width: size.width * 0.4,
+        ),
+        leading: const CircleAvatar(
+            backgroundImage: AssetImage(
+          "assets/images/avatar-profile.png",
+        )),
+      ),
     );
   }
 }
